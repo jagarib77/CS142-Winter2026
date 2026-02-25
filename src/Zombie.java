e510b216db3fe2bb3403217954265d28757a7a7c
Phuong-Ton <phuongtonthat@gmail.com>
2026-02-24 23:19:35 -0800

    Create the map from file, then Add listener for button

--- src/Zombie.java
+++
@@ -1,5 +0,0 @@
-public class Zombie{
-    public static void main(String[] args){
-        System.out.println("DEMO GIT 2");
-    }
-}
---
+++ src/xZombie.java
@@ -0,0 +1,3 @@
+public class xZombie{
+    public xZombie(){}
+}
