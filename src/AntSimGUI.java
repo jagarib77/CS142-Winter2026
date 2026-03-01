bf6c1168b6a5da4c2094e0dff26b26cd4381024d
KyleHamasaki <hamhammmerham@gmail.com>
2026-02-28 18:04:20 -0800

    Update AntSimGUI.java
    Buttons are functional, but I have not created the animation yet.

--- src/AntSimGUI.java
+++ src/AntSimGUI.java
@@ -3,14 +3,18 @@
 // By Kyle Hamasaki
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.*;
 
 public class AntSimGUI extends JFrame {
     // The size of the square cells containing the Terrain, Ant, and WorldObject
     private final int cellSize = 10;
-    // The WorldGrid
-    private WorldGrid grid;
+    // The Ant simulator
+    private AntSim simulator;
     // The entire interface of the program
     private JFrame frame;
     // A label that contains an image of the world
@@ -18,15 +22,17 @@ public class AntSimGUI extends JFrame {
     // Used to draw in imageContainer
     private Graphics g;
 
-    public AntSimGUI() {
-        this.grid = grid;
+    public AntSimGUI(AntSim simulator) {
+        this.simulator = simulator;
         frame = new JFrame();
         frame.setLayout(new BorderLayout());
         frame.setTitle("Ant Simulator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        // The WorldGrid based on simulator
+        WorldGrid world = simulator.getWorld();
         // Creates an area on the frame for the animation.
-        BufferedImage worldImage = new BufferedImage(grid.getWidth() * cellSize, grid.getHeight() * cellSize,
+        BufferedImage worldImage = new BufferedImage(world.getWidth() * cellSize, world.getHeight() * cellSize,
                 BufferedImage.TYPE_INT_RGB);
 
         imageContainer = new JLabel(new ImageIcon(worldImage));
@@ -34,15 +40,32 @@ public class AntSimGUI extends JFrame {
 
         // Creates an area on the frame for the buttons.
         // Note that the buttons do not do anything yet, and that more buttons can be added.
-        JPanel buttonContainer = new JPanel(new FlowLayout());
+        JPanel buttonContainer = new JPanel();
+        // The buttons that will be added to the GUI
+        List<JButton> buttons = new ArrayList<>();
+        ActionListener listener = new MyActionListener();
 
-        // Button that speeds up the animation.
-        JButton speedUpButton = new JButton("Speed Up");
-        buttonContainer.add(speedUpButton);
+        // Button that stops the animation
+        buttons.add(new JButton("Stop"));
 
-        // Button that slows down the animation.
-        JButton slowDownButton = new JButton("Slow down");
-        buttonContainer.add(slowDownButton);
+        // Button that starts the animation
+        buttons.add(new JButton("Start"));
+
+        // Button that ticks the animation to the next step
+        buttons.add(new JButton("Tick"));
+
+        // Button that speeds up the animation
+        buttons.add(new JButton("Speed up"));
+
+        // Button that slows down the animation
+        buttons.add(new JButton("Slow down"));
+
+        // Adds all the buttons into buttonContainer and makes them functional.
+        for (JButton button : buttons) {
+            buttonContainer.add(button);
+            button.addActionListener(listener);
+        }
+        buttonContainer.setLayout(new GridLayout(1, -1));
 
         frame.add(imageContainer, BorderLayout.CENTER);
         frame.add(buttonContainer, BorderLayout.SOUTH);
@@ -51,48 +74,78 @@ public class AntSimGUI extends JFrame {
 
         drawWorld();
         frame.setVisible(true);
+
     }
 
     // Draws the WorldGrid as a picture.
     // By Kyle Hamasaki
     public void drawWorld() {
-        for (int i = 0; i < grid.getWidth(); i++) {
-            for (int j = 0; j < grid.getHeight(); j++) {
+        // The WorldGrid based on simulator
+        WorldGrid world = simulator.getWorld();
+
+        for (int i = 0; i < world.getWidth(); i++) {
+            for (int j = 0; j < world.getHeight(); j++) {
                 Point currentLocation = new Point(i, j);
 
                 // Draws the Terrain
-                Terrain terrain = grid.getTerrainAt(currentLocation);
-                String terrainName = terrain.getSymbol();
-                if (terrainName.equals("#")) {
+                Terrain terrain = world.getTerrainAt(currentLocation);
+                char terrainSymbol = terrain.getSymbol();
+                if (terrainSymbol == '#') {
                     // The color if the Terrain is Dirt
                     g.setColor(new Color(0x785B4C));
                     // The color if the Terrain is Rock
-                } else if (terrainName.equals("R")) {
-                    g.setColor(new Color(0xEA6C6E6E, true));
-                } else if (terrainName.equals(".")) {
+                } else if (terrainSymbol == 'R') {
+                    g.setColor(new Color(0xFF6C6E6E));
+                } else if (terrainSymbol == '.') {
                     // The color if the Terrain is Tunnel
                     g.setColor(new Color(0x514031));
                 } else {
                     // The color if the Terrain is Air/Terrain
-                    g.setColor(new Color(0xFFFFFF));
+                    g.setColor(new Color(0x74A5EF));
                 }
-                g.fillRect(i, j, cellSize, cellSize);
+                g.fillRect(i * cellSize, j * cellSize, cellSize, cellSize);
 
                 // Draws the WorldObject
-                WorldObject worldObject = grid.getObjectAt(currentLocation);
-                String worldObjectName = worldObject.getSymbol();
-                // Draws the character of the WorldObject in the middle of the cell.
-                g.drawString(worldObjectName, 5 + (i * cellSize), 5 + (j * cellSize));
-
-                // I am unsure how to draw the Ants.
+                WorldObject worldObject = world.getObjectAt(currentLocation);
+                if (worldObject != null) {
+                    char worldObjectSymbol = worldObject.getSymbol();
+                    // Draws the character of the WorldObject in the middle of the cell.
+                    g.drawString(Character.toString(worldObjectSymbol), (cellSize / 2) + (i * cellSize), (cellSize / 2) + (j * cellSize));
+                }
             }
+        }
+
+        // Draws the Ants
+        List<Ant> ants = simulator.getAnts();
+        for (Ant a : ants) {
+            int xPos = a.getX();
+            int yPos = a.getY();
+            g.drawString(Character.toString(a.getSymbol()), (cellSize / 2) + (xPos * cellSize), (cellSize / 2) + (yPos * cellSize));
         }
     }
 
     // Updates the WorldGrid and draws a new picture of the WorldGrid.
     public void update() {
-        // Maybe WorldGrid should have an update() method, which this method could call.
-        // grid.update();
+        simulator.step();
         drawWorld();
+        frame.repaint();
+    }
+
+    // Used to make buttons functional. The buttons do not do their respective function yet.
+    //TODO: create a timer and make the buttons perform their respective functions
+    public class MyActionListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            if (event.getActionCommand().equals("Stop")) {
+                System.out.println("Stop");
+            } else if (event.getActionCommand().equals("Start")) {
+                System.out.println("Start");
+            } else if (event.getActionCommand().equals("Tick")) {
+                System.out.println("Tick");
+            } else if (event.getActionCommand().equals("Speed up")) {
+                System.out.println("Speed up");
+            } else if (event.getActionCommand().equals("Slow down")) {
+                System.out.println("Slow down");
+            }
+        }
     }
 }
