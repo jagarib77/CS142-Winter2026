0cafbe0bc35c3f428a60a157f5b4d8038082f3af
Rita Hsiao <chao522957@gmail.com>
2026-02-28 23:15:49 -0800

    Add SimulationGUI.java to GUI branch

--- src/main/java/SimulationGUI.java
+++ src/main/java/SimulationGUI.java
@@ -1,15 +1,21 @@
 //SimulationGui.java
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
 import java.awt.Graphics;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SimulationGUI extends JPanel{
     
     private SimulationModel model;
     //one block's size is 20
     private int cellSize=20;
+
+    private Timer timer;
+    private int turn=500; 
 
     public SimulationGUI(SimulationModel model){
         this.model=model;
@@ -20,6 +26,39 @@ public class SimulationGUI extends JPanel{
         int r=grid.length;
         int c=grid[0].length;
         setPreferredSize(new Dimension(c*cellSize, r*cellSize));
+
+        ActionListener listener=new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                model.update();
+                repaint();
+            }
+        };
+
+        timer=new Timer(turn, listener);
+    }
+    
+    public void startSimulation(){
+        timer.start();
+    }
+
+    public void pauseSimulation(){
+        timer.stop();
+    }
+
+    public void restartSimulation(){
+        timer.stop();
+        model.reset();
+        repaint();
+    }
+
+    public void setParameters(int rows, int cols, int time){
+        timer.stop();
+
+        this.turn=time;
+        timer.setDelay(time);
+
+        model.initialize(rows, cols);
+        repaint();
     }
 
     @Override
@@ -39,7 +78,7 @@ public class SimulationGUI extends JPanel{
 
                 //all zombie is red
                 else if(grid[x][y] instanceof Zombie){
-                    g.setColor(Color.GREEN);
+                    g.setColor(Color.RED);
                 }
 
                 //not thing this block show white
@@ -53,7 +92,7 @@ public class SimulationGUI extends JPanel{
                 if(grid[x][y] instanceof LivingEntity){
                     LivingEntity le=(LivingEntity)grid[x][y];
                     g.setColor(Color.BLACK);
-                    g.drawString(""+le.getHealth(), y*cellSize+5, x*cellSize+15);
+                    g.drawString(""+le.getHealth(), y*cellSize+5, x*cellSize+10);
                 }
 
                 //draw edge
