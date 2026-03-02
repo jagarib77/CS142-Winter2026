2d0156ba0bece9592e86ce8f1f9ee8c27b179e80
KyleHamasaki <hamhammmerham@gmail.com>
2026-03-01 17:40:53 -0800

    Update AntSim.java
    Worked on step(), Also added some user interactivity in console (but it does not do anything yet).

--- src/AntSim.java
+++ src/AntSim.java
@@ -2,7 +2,7 @@
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class AntSim {
     private final WorldGrid world;
@@ -29,6 +29,7 @@ public class AntSim {
         System.out.println("Welcome to the Ant Colony Simulator!");
         System.out.println("This program creates a world with ant colonies and simulates ants'");
         System.out.println("behaviors like foraging, fighting, and procreating.");
+        System.out.println();
     }
 
     public WorldGrid getWorld() { return world; }
@@ -42,6 +43,26 @@ public class AntSim {
         // a new ant and .add() it to the ant list
 
         //TODO: remove dead ants from ant list
+
+        // Removes dead ants and makes queen ants spawn other ants, but does not make them move
+        // - Kyle
+        for (int i = ants.size() - 1; i >= 0; i--) {
+            Ant ant = ants.get(i);
+
+            // Removes the Ant from the list if it is not alive.
+            if (!ant.isAlive()) {
+                ants.remove(i);
+            }
+
+            // If the Ant is a Queen Ant, then the Queen Ant will try to spawn a new Ant, which
+            // will be added to the list.
+            if (ant.getSymbol() == 'Q') {
+                Ant newAnt = ((QueenAnt) ant).spawnAnt();
+                if (newAnt != null) {
+                    ants.add(newAnt);
+                }
+            }
+        }
 
         //TODO: implement logic for ants to move
         // don't worry about this yet, we need to get pheromones working
@@ -90,12 +111,51 @@ public class AntSim {
         //TODO: need to spawn the first ants in the sim (at least a queen)
     }
 
+    // Asks for and returns a number between start and end (inclusive).
+    public static int getIntegerBetweenRange(Scanner console, int start, int end) {
+        System.out.print("Input an integer between " + start + " to " + end + " (inclusive): ");
+        String userInput = console.next();
+        try {
+            int num = Integer.parseInt(userInput);
+
+            if (num >= start && num <= end) {
+                System.out.println();
+                return num;
+            } else {
+                System.out.println("Integer not in range.");
+                return getIntegerBetweenRange(console, start, end);
+            }
+        } catch (Exception e){
+            System.out.println("Invalid input.");
+            return getIntegerBetweenRange(console, start, end);
+        }
+    }
+
     // main file - runs the program
     // create the world sim then pass that world to AntSimGUI.java to run the
     // GUI so we can see what happens
     public static void main(String[] args) {
-        printIntro();
         AntSim sim = new AntSim(50, 50);
+
+        printIntro();
+
+        /*
+        I did not know if we want to make the user be able to input some variables, like the size
+        of the simulation and the number of ants, but I included this if you do want to. If you
+        do not want to, then you can delete this. - Kyle
+        Scanner console = new Scanner(System.in);
+
+        System.out.println("How many queen ants would you like?");
+        int queenAntNum = getIntegerBetweenRange(console, 1, 3);
+
+        System.out.println("How many guard ants would you like?");
+        int guardAntNum = getIntegerBetweenRange(console, 1, 35);
+
+        System.out.println("How many worker ants would you like?");
+        int workerAntNum = getIntegerBetweenRange(console, 1, 70);
+        */
+
         new AntSimGUI(sim);
+
     }
 }
