0cf8fda97c28d17d0cf4e29b68073ee1c12f9653
Rita Hsiao <chao522957@gmail.com>
2026-03-07 17:43:29 -0800

    Change Greetings

--- src/main/java/Greetings.java
+++ src/main/java/Greetings.java
@@ -11,6 +11,22 @@ public final class Greetings {
         // students add exactly one line each:
         // MESSAGES.add("Hola — Alice P.");
         MESSAGES.add("Hello-DarionP.");
+
+        MESSAGES.add("Human vs. Zombie Survival Simulation is an infection and survival simulation in grid-based. ");
+        MESSAGES.add("The simulation takes place in a two-dimensional world divided into cells with integer coordinates, similar to the Critter model. Each cell may contain an entity. ");
+
+        MESSAGES.add("Humans move randomly in the grid and exist in different states, healthy or infected. ");
+        MESSAGES.add("When an infected human encounters a healthy human, there is a probability that the infection will spread. ");
+        MESSAGES.add("If an infection persists for a certain amount of time, the human may transform into a zombie.");
+
+        MESSAGES.add("Zombies actively move through the grid and attempt to infect or attack nearby humans. ");
+        MESSAGES.add("Some specialized humans, such as doctors, are able to heal infected individuals, reduce the spread of infection. ");
+        MESSAGES.add("Different types of humans and zombies exhibit different behaviors, allowing the simulation to demonstrate how variation among agents affects overall survival. ");
+        
+        MESSAGES.add("The simulation runs as an animated graphical user interface that updates over time ");
+        MESSAGES.add("Users can control key parameters such as population size. ");
+        MESSAGES.add("The simulation ends when either no zombies remain or when all humans have become infected or transformed. ");
+        
     }
 
     /** Returns an unmodifiable view of the greetings list. */
