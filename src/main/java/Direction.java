1ba2fee38107e26f4b837c4e5af4bd9ebe9816f2
KyleHamasaki <hamhammmerham@gmail.com>
2026-03-08 21:20:44 -0700

    Update Direction.java
    Removed the moveToPoint() and moveAwayFromPoint methods because they seemed like a better fit in the Point class.

--- src/main/java/Direction.java
+++ src/main/java/Direction.java
@@ -1,7 +1,7 @@
 // Direction.java
 // Cardinal movement directions used by ants and pheromone spreading.
 // Group Project: Ant Colony Simulator
-// Authors: Harrison Butler and Kyle Hamasaki
+// Authors: Harrison Butler
 
 import java.util.Random;
 
@@ -41,52 +41,5 @@ public enum Direction {
      */
     public static Direction[] allDirections(){
         return new Direction[]{NORTH, SOUTH, EAST, WEST};
-    }
-
-    // Returns a Direction to move from the current Point to the target Point.
-    // By Kyle
-    public static Direction moveToPoint(Point current, Point target) {
-        int dx = target.x - current.x;
-        int dy = target.y - current.y;
-
-        // If the current Point and target Point are at the same location, then CENTER will
-        // be returned.
-        if (dx == 0 && dy == 0) {
-            return CENTER;
-        }
-
-        // If the magnitudes of the horizontal and vertical distances between the Points are
-        // equal, then it will return a random Direction towards the target.
-        if (Math.abs(dx) == Math.abs(dy)) {
-            if (Math.random() >= 0.5) {
-                dx = 0;
-            } else {
-                dy = 0;
-            }
-        }
-
-        // If the magnitude of the horizontal distance between the Points is larger than the
-        // vertical, then it will return a horizontal Direction towards the target.
-        if (Math.abs(dx) > Math.abs(dy)) {
-            if (dx < 0) {
-                return WEST;
-            } else {
-                return EAST;
-            }
-        } else {
-            // If the magnitude of the vertical distance between the Points is larger than the
-            // horizontal, then it will return a vertical Direction towards the target.
-            if (dy < 0) {
-                return NORTH;
-            } else {
-                return SOUTH;
-            }
-        }
-    }
-    
-    // Returns a Direction to move from the current Point away from the target Point.
-    // By Kyle
-    public static Direction moveAwayFromPoint(Point current, Point target) {
-        return moveToPoint(current, new Point(-target.x, -target.y));
     }
 }
