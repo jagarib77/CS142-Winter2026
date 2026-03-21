e49f76dca9bdb1479fdbb85fcab98c41bdb5cbea
KyleHamasaki <hamhammmerham@gmail.com>
2026-03-20 21:24:56 -0700

    Added another constructor for ColonyAnt
    Added constructor with more parameters for loading in a file

--- src/main/java/sim/ColonyAnt.java
+++ src/main/java/sim/ColonyAnt.java
@@ -4,6 +4,7 @@ package sim;// sim.ColonyAnt.java
 // Authors: Harrison Butler
 
 import pheromones.PheromoneType;
+import resources.WorldObject;
 import util.Direction;
 import util.Point;
 
@@ -33,7 +34,36 @@ public class ColonyAnt extends Ant {
         if (home == null) throw new IllegalArgumentException("home");
         this.home = home;
         this.colonyID = colonyID;
+        setFoodStore(home);
     }
+
+    /**
+     * Initializes a colony ant with a given starting position, maximum energy capacity, current
+     * energy, held item, whether the ant is alive, its previous locations, its home's location,
+     * its colonyID, and its food storage's location.
+     *
+     * @param world world the ant lives in (non-null)
+     * @param rng random generator used for decisions (non-null)
+     * @param pos initial position (non-null)
+     * @param maxEnergy maximum energy capacity, must be > 0
+     * @param heldItem the WorldObject the nt is holding
+     * @param alive boolean for whether the ant is alive
+     * @param lastLocations list storing ant's last locations in the world
+     * @param home location of the home point (non-null)
+     * @param colonyID a number used to identify ants of its own colony
+     * @param foodStore location of the food store (always at home point currently)
+     */
+    public ColonyAnt(WorldGrid world, Random rng, Point pos, int maxEnergy, int currentEnergy,
+               WorldObject heldItem, boolean alive, List<Point> lastLocations, Point home,
+                     int colonyID, Point foodStore) {
+        super(world, rng, pos, maxEnergy, currentEnergy, heldItem, alive, lastLocations);
+
+        if (home == null) throw new IllegalArgumentException("home");
+        this.home = home;
+        this.colonyID = colonyID;
+        this.foodStore = foodStore;
+    }
+
 
     public final Point getHome() { return home; }
 
