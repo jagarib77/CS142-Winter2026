fe4e3fb04e94880595f58c8d7a6be31eee448ac0
KyleHamasaki <hamhammmerham@gmail.com>
2026-03-20 21:30:28 -0700

    Added another constructor
    Added another constructor with more parameters for loading in a file.

--- src/main/java/sim/QueenAnt.java
+++ src/main/java/sim/QueenAnt.java
@@ -3,9 +3,11 @@ package sim;// sim.QueenAnt.java
 // Group Project: sim.Ant Colony Simulator
 // Authors: Harrison Butler
 
+import resources.WorldObject;
 import util.Direction;
 import util.Point;
 
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -18,6 +20,28 @@ public class QueenAnt extends ColonyAnt {
      */
     public QueenAnt(WorldGrid world, Random rng, Point pos, int maxEnergy, Point home, int ColonyID){
         super(world, rng, pos, maxEnergy, home, ColonyID);
+    }
+
+    /**
+     * Initializes a queen ant with a given starting position, maximum energy capacity, current
+     * energy, held item, whether the ant is alive, its previous locations, its home's location,
+     * its colonyID, and its food storage's location.
+     *
+     * @param world world the ant lives in (non-null)
+     * @param rng random generator used for decisions (non-null)
+     * @param pos initial position (non-null)
+     * @param maxEnergy maximum energy capacity, must be > 0
+     * @param heldItem the WorldObject the nt is holding
+     * @param alive boolean for whether the ant is alive
+     * @param lastLocations list storing ant's last locations in the world
+     * @param home location of the home point
+     * @param colonyID a number used to identify ants of its own colony
+     * @param foodStore location of the food store (always at home point currently)
+     */
+    public QueenAnt(WorldGrid world, Random rng, Point pos, int maxEnergy, int currentEnergy,
+                    WorldObject heldItem, boolean alive, List<Point> lastLocations, Point home,
+                    int colonyID, Point foodStore) {
+        super(world, rng, pos, maxEnergy, currentEnergy, heldItem, alive, lastLocations, home, colonyID, foodStore);
     }
 
     /**
